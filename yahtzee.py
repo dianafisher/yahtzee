0546d7faac6da3a8953bfccdedefe81ebe07bcbe
Diana Fisher <diana.fisher@gmail.com>
2016-07-27 16:40:58 -0700

    * Obtain the user and game entities from the roll in score_roll endpoint.

--- yahtzee.py
+++ yahtzee.py
@@ -105,8 +105,8 @@ class YahtzeeApi(remote.Service):
         if game.game_over:
             raise endpoints.NotFoundException('Game already over')
 
-        if game.has_unscored_roll:
-            raise endpoints.ConflictException('Cannot roll again until current roll has been scored.  Please score current roll.')
+        # if game.has_unscored_roll:
+        #     raise endpoints.ConflictException('Cannot roll again until current roll has been scored.  Please score current roll.')
         
         game.has_unscored_roll = True
         game.put()
@@ -145,6 +145,15 @@ class YahtzeeApi(remote.Service):
             raise endpoints.NotFoundException('Roll not found')
 
         category_type = request.category_type
+        print 'roll', roll
+        user = roll.user.get()
+        print 'user', user
+        game = roll.game.get()
+        print 'game', game
+        # Get the user's scorecard for this game.
+        scorecard = ScoreCard.query(ndb.AND(ScoreCard.user == user.key, ScoreCard.game == game.key )).get()
+        print 'scorecard', scorecard
+
         return roll.calculate_score(category_type)
 
 # Score Card
