cfe117916470944cf071bebb586687135216abad
Diana Fisher <diana.fisher@gmail.com>
2016-09-06 13:54:57 -0700

    Adjust formatting to satisfy PEP8 requirements.

--- models.py
+++ models.py
@@ -1,12 +1,13 @@
+import random
+from datetime import date
+from protorpc import messages
+from google.appengine.ext import ndb
+
 """
 models.py - This file contains the class definitions for the Datastore
 entities used by the game.
 """
 
-import random
-from datetime import date
-from protorpc import messages
-from google.appengine.ext import ndb
 
 class User(ndb.Model):
     """ User object """
@@ -20,11 +21,12 @@ class User(ndb.Model):
                         email=self.email)
         return form
 
+
 class Game(ndb.Model):
     """ Game object """
     game_over = ndb.BooleanProperty(required=True, default=False)
     user = ndb.KeyProperty(required=True, kind='User')
-    history = ndb.PickleProperty(required=True)    
+    history = ndb.PickleProperty(required=True)
     has_unscored_roll = ndb.BooleanProperty(required=True)
 
     @classmethod
@@ -32,27 +34,28 @@ class Game(ndb.Model):
         """Creates and returns a new game"""
         game = Game(user=user)
         game.history = []
-        game.has_unscored_roll = False    
+        game.has_unscored_roll = False
         game.put()
         return game
 
     def to_form(self):
         """Returns a GameForm representation of the Game"""
         form = GameForm(urlsafe_key=self.key.urlsafe(),
-                        user_name=self.user.get().name,                        
+                        user_name=self.user.get().name,
                         game_over=self.game_over,
                         has_unscored_roll=self.has_unscored_roll)
-        return form    
+        return form
 
     def end_game(self, won=False):
         """Ends the game - if won is True, the player won. - if won is False,
         the player lost."""
-        self.game_over = True        
+        self.game_over = True
         self.put()
         # # Add the game to the score 'board'
         # score = Score(user=self.user, date=date.today(), won=won,
-        #               guesses=self.attempts_allowed - self.attempts_remaining)
+        #           guesses=self.attempts_allowed - self.attempts_remaining)
         # score.put()
+
 
 class Score(ndb.Model):
     """Score object"""
@@ -65,59 +68,32 @@ class Score(ndb.Model):
         return ScoreForm(user_name=self.user.get().name, won=self.won,
                          date=str(self.date), guesses=self.guesses)
 
-# class Turn(ndb.Model):
-#     """Turn object - Each turn consits of up to three rolls."""
-#     user = ndb.KeyProperty(required=True, kind='User')
-#     game = ndb.KeyProperty(required=True, kind='Game')
-#     roll_count = ndb.IntegerProperty(required=True, default=0)
-#     dice = ndb.PickleProperty(required=True)
 
-#     @classmethod
-#     def new_turn(cls, user, game):
-#         """Creates a new turn"""
-#         turn = Turn(user=user, game=game)
-#         turn.dice = []
-#         turn.put()
-#         return turn
-
-#     def roll_dice(self, keepers):
-#         if self.roll_count == 3:
-#             raise ValueError('Cannot roll dice more than three times in one turn')
-
-#         for i in range(5):
-#             value = random.choice(range(1, 7))
-#             self.dice.append(value)
-#             self.roll_count += 1
-#             print value
-    
-#         self.put()
-
-# Forms/Messages
+# Forms
 
 class UserForm(messages.Message):
-    """UserForm for outbound user information"""    
+    """UserForm for outbound user information"""
     urlsafe_key = messages.StringField(1, required=True)
     user_name = messages.StringField(2, required=True)
     email = messages.StringField(3, required=True)
+
 
 class UserForms(messages.Message):
     """Form to return a list of all users"""
     users = messages.MessageField(UserForm, 1, repeated=True)
 
+
 class GameForm(messages.Message):
     """GameForm for outbound game state information"""
-    urlsafe_key = messages.StringField(1, required=True)    
-    game_over = messages.BooleanField(2, required=True)    
+    urlsafe_key = messages.StringField(1, required=True)
+    game_over = messages.BooleanField(2, required=True)
     user_name = messages.StringField(3, required=True)
     has_unscored_roll = messages.BooleanField(4, required=True)
+
 
 class GameForms(messages.Message):
     """Form to return list of games"""
     games = messages.MessageField(GameForm, 1, repeated=True)
-
-# class NewGameForm(messages.Message):
-#     """Used to create a new game"""
-#     user_name = messages.StringField(1, required=True)    
 
 
 class MakeMoveForm(messages.Message):
@@ -140,5 +116,4 @@ class ScoreForms(messages.Message):
 
 class StringMessage(messages.Message):
     """StringMessage-- outbound (single) string message"""
-    message = messages.StringField(1, required=True)    
-
+    message = messages.StringField(1, required=True)
--- yahtzee.py
+++ yahtzee.py
@@ -1,9 +1,3 @@
-"""
-yahtzee.py - Create and configure the game API.
-
-"""
-__author__ = 'diana.fisher@gmail.com (Diana Fisher)'
-
 import endpoints
 from protorpc import messages
 from protorpc import message_types
@@ -18,27 +12,36 @@ from google.appengine.ext import ndb
 from models import User, Game, Score
 from models import StringMessage, GameForm, GameForms, UserForm, UserForms
 
-from roll import Roll, RollDiceForm, RollResultForm, ScoreRollForm, ScoreRollResultForm, RerollDiceForm
-
-from scorecard import CategoryType, ScoreCard, ScoreCardRequestForm, ScoreCardForm
+from roll import Roll, RollDiceForm, RollResultForm
+from roll import ScoreRollForm, ScoreRollResultForm, RerollDiceForm
+from scorecard import CategoryType, ScoreCard
+from scorecard import ScoreCardRequestForm, ScoreCardForm
 
 from utils import get_by_urlsafe
+
+"""
+yahtzee.py - Create and configure the game API.
+
+"""
+__author__ = 'diana.fisher@gmail.com (Diana Fisher)'
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 USER_REQUEST = endpoints.ResourceContainer(user_name=messages.StringField(1),
                                            email=messages.StringField(2))
 
-NEW_GAME_REQUEST = endpoints.ResourceContainer(user_name = messages.StringField(1, required=True))
+NEW_GAME_REQUEST = endpoints.ResourceContainer(
+    user_name=messages.StringField(1, required=True))
 
-USER_GAMES_REQUEST = endpoints.ResourceContainer(user_name=messages.StringField(1))
+USER_GAMES_REQUEST = endpoints.ResourceContainer(
+    user_name=messages.StringField(1))
 
 ROLL_DICE_REQUEST = endpoints.ResourceContainer(
     RollDiceForm,
     urlsafe_game_key=messages.StringField(1),)
 
 SCORE_ROLL_REQUEST = endpoints.ResourceContainer(
-    ScoreRollForm,    
+    ScoreRollForm,
     urlsafe_roll_key=messages.StringField(1),)
 
 REROLL_DICE_REQUEST = endpoints.ResourceContainer(
@@ -46,8 +49,8 @@ REROLL_DICE_REQUEST = endpoints.ResourceContainer(
     urlsafe_roll_key=messages.StringField(1),)
 
 SCORECARD_REQUEST = endpoints.ResourceContainer(
-  ScoreCardRequestForm,
-  urlsafe_game_key=messages.StringField(1),)
+    ScoreCardRequestForm,
+    urlsafe_game_key=messages.StringField(1),)
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
@@ -56,11 +59,11 @@ SCORECARD_REQUEST = endpoints.ResourceContainer(
 #                 allowed_client_ids=[WEB_CLIENT_ID, API_EXPLORER_CLIENT_ID],
 #                 scopes=[EMAIL_SCOPE])
 
-@endpoints.api( name='yahtzee', version='v1' )
+
+@endpoints.api(name='yahtzee', version='v1')
 class YahtzeeApi(remote.Service):
     """Yahtzee API v0.1"""
 
-# Users
     # Create user
     @endpoints.method(request_message=USER_REQUEST,
                       response_message=UserForm,
@@ -71,23 +74,22 @@ class YahtzeeApi(remote.Service):
         """Create a User. Requires a unique username"""
         if User.query(User.name == request.user_name).get():
             raise endpoints.ConflictException(
-                    'A User with that name already exists!')
+                'A User with that name already exists!')
         user = User(name=request.user_name, email=request.email)
         user.put()
         # return StringMessage(message='User {} created!'.format(
         #         request.user_name))
         return user.to_form()
 
-    # Get users
+    # Get all users
     @endpoints.method(response_message=UserForms,
                       path='user/list',
                       name='get_users',
                       http_method='GET')
     def get_users(self, request):
-      """Returns list of all users."""      
-      return UserForms(users=[user.to_form() for user in User.query()])
+        """Returns list of all users."""
+        return UserForms(users=[user.to_form() for user in User.query()])
 
-# Game
     # Create new game for user
     @endpoints.method(request_message=NEW_GAME_REQUEST,
                       response_message=GameForm,
@@ -100,7 +102,7 @@ class YahtzeeApi(remote.Service):
         user = User.query(User.name == request.user_name).get()
         if not user:
             raise endpoints.NotFoundException(
-                    'A User with that name does not exist!')
+                'A User with that name does not exist!')
 
         game = Game.new_game(user.key)
         score_card = ScoreCard.new_scorecard(user.key, game.key)
@@ -113,16 +115,19 @@ class YahtzeeApi(remote.Service):
                       name='get_user_games',
                       http_method='POST')
     def get_user_games(self, request):
-      # Query for a user with this user name.
-      user = User.query(User.name == request.user_name).get()
-      if not user:
-          message = ('User {} not found!').format(request.user_name)
-          raise endpoints.NotFoundException(message)
-      # Query for all active games for this user.
-      games = Game.query(Game.user == user.key).filter(Game.game_over == False)
-      return GameForms(games=[game.to_form() for game in games])
+        # Query for a user with this user name.
+        user = User.query(User.name == request.user_name).get()
+        if not user:
+            message = ('User {} not found!').format(request.user_name)
+            raise endpoints.NotFoundException(message)
+        # Query for all active games for this user.
+        games = Game.query(Game.user == user.key).filter(
+            Game.game_over == False)
+        return GameForms(games=[game.to_form() for game in games])
 
-# Roll Dice
+    # Cancel game
+
+    # Roll Dice
     @endpoints.method(request_message=ROLL_DICE_REQUEST,
                       response_message=RollResultForm,
                       path='game/{urlsafe_game_key}/roll',
@@ -138,7 +143,7 @@ class YahtzeeApi(remote.Service):
 
         # if game.has_unscored_roll:
         #     raise endpoints.ConflictException('Cannot roll again until current roll has been scored.  Please score current roll.')
-        
+
         game.has_unscored_roll = True
         game.put()
 
@@ -154,15 +159,15 @@ class YahtzeeApi(remote.Service):
                       http_method='PUT')
     def reroll_dice(self, request):
         """Method to roll dice again after first roll"""
-                
+
         roll = get_by_urlsafe(request.urlsafe_roll_key, Roll)
         if not roll:
             raise endpoints.NotFoundException('Roll not found')
         if roll.count == 3:
-            raise endpoints.ConflictException('Cannot roll more than 3 times in a single turn.  Please score roll instead.')
+            raise endpoints.ConflictException(
+                'Cannot roll more than 3 times in a single turn.  Please score roll instead.')
 
         return roll.reroll(request.keepers)
-
 
     @endpoints.method(request_message=SCORE_ROLL_REQUEST,
                       response_message=ScoreCardForm,
@@ -170,7 +175,7 @@ class YahtzeeApi(remote.Service):
                       name='score_roll',
                       http_method='PUT')
     def score_roll(self, request):
-        """Score roll"""        
+        """Score roll"""
         roll = get_by_urlsafe(request.urlsafe_roll_key, Roll)
         if not roll:
             raise endpoints.NotFoundException('Roll not found')
@@ -189,9 +194,10 @@ class YahtzeeApi(remote.Service):
         # print 'current_score:', current_score
 
         if current_score > -1:
-          message = ('{} category already contains a score.  Please select a different score category.').format(str(category_type))
-          # print message
-          raise endpoints.ConflictException(message)
+            message = ('{} category already contains a score.  Please select a different score category.').format(
+                str(category_type))
+            # print message
+            raise endpoints.ConflictException(message)
 
         return roll.calculate_score(category_type)
 
@@ -202,19 +208,21 @@ class YahtzeeApi(remote.Service):
                       name='score_card',
                       http_method='POST')
     def score_card(self, request):
-      """Returns the user's scorecard for the game"""
-      user = User.query(User.name == request.user_name).get()
-      if not user:
-        raise endpoints.NotFoundException('A User with that name does not exist!')
+        """Returns the user's scorecard for the game"""
+        user = User.query(User.name == request.user_name).get()
+        if not user:
+            raise endpoints.NotFoundException(
+                'A User with that name does not exist!')
 
-      game = get_by_urlsafe(request.urlsafe_game_key, Game)
-      if not game:
-        raise endpoints.NotFoundException('Game not found')
+        game = get_by_urlsafe(request.urlsafe_game_key, Game)
+        if not game:
+            raise endpoints.NotFoundException('Game not found')
 
-      scorecard = ScoreCard.query(ndb.AND(ScoreCard.user == user.key, ScoreCard.game == game.key )).get()
-      print 'scorecard', scorecard
-      return scorecard.to_form()
-        
+        scorecard = ScoreCard.query(
+            ndb.AND(ScoreCard.user == user.key, ScoreCard.game == game.key)).get()
+        print 'scorecard', scorecard
+        return scorecard.to_form()
+
 
 # registers API
-api = endpoints.api_server([YahtzeeApi])         
+api = endpoints.api_server([YahtzeeApi])
