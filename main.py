898609c89b3ad85ef54b92c76aa444964ba19781
Diana Fisher <diana.fisher@gmail.com>
2016-09-18 22:05:57 -0700

    Update cron job to send reminder email to users with games in progress.

--- main.py
+++ main.py
@@ -6,20 +6,32 @@ import logging
 
 import webapp2
 from google.appengine.api import mail, app_identity
-from api import GuessANumberApi
+# from api import GuessANumberApi
 
 from models import User
+from game import Game
 
 
 class SendReminderEmail(webapp2.RequestHandler):
     def get(self):
-        """Send a reminder email to each User with an email about games.
+        """Send a reminder email to each User with an email who has
+        games in progress. Email body includes a count of active games and their
+        urlsafe keys
         Called every hour using a cron job"""
         app_id = app_identity.get_application_id()
         users = User.query(User.email != None)
         for user in users:
-            subject = 'This is a reminder!'
-            body = 'Hello {}, try out Guess A Number!'.format(user.name)
+            # Query for Users which have games in progress.
+            games = Game.query(Game.user == user.key).\
+                filter(Game.game_over == False)
+            if games.count() > 0:
+                subject = 'This is a reminder!'
+                body = 'Hello {}, you have {} games in progress. Their' \
+                       ' keys are: {}'.\
+                       format(user.name, games.count()
+                        ,', '.join(game.key.urlsafe() for game in games))
+                logging.debug(body)
+
             # This will send test emails, the arguments to send_mail are:
             # from, to, subject, body
             mail.send_mail('noreply@{}.appspotmail.com'.format(app_id),
@@ -28,14 +40,14 @@ class SendReminderEmail(webapp2.RequestHandler):
                            body)
 
 
-class UpdateAverageMovesRemaining(webapp2.RequestHandler):
-    def post(self):
-        """Update game listing announcement in memcache."""
-        GuessANumberApi._cache_average_attempts()
-        self.response.set_status(204)
+# class UpdateAverageMovesRemaining(webapp2.RequestHandler):
+    # def post(self):
+        # """Update game listing announcement in memcache."""
+#         GuessANumberApi._cache_average_attempts()
+        # self.response.set_status(204)
 
 
 app = webapp2.WSGIApplication([
     ('/crons/send_reminder', SendReminderEmail),
-    ('/tasks/cache_average_attempts', UpdateAverageMovesRemaining),
+    # ('/tasks/cache_average_attempts', UpdateAverageMovesRemaining),
 ], debug=True)
